f3b65598b87c187874fb4742eebdd9450ca88aee
Lucio109 <137243411+Lucio109@users.noreply.github.com>
2023-10-11 12:26:41 -0300

    initialcommit

---
+++ clase3/index.php
@@ -0,0 +1,39 @@
+<?php
+// Ejercicio 1
+print "Hola mundo"."<br>";
+// Ejercicio 2
+$mensaje = "Hola mundo";
+print $mensaje ."<br>";
+// Ejercicio 3
+$numero1 = 10;
+$numero2 = 5;
+$suma = $numero1 + $numero2;
+$resta = $numero1 - $numero2;
+$multiplicacion = $numero1 * $numero2;
+$division_entera = $numero1 / $numero2;
+$resto = $numero1 % $numero2;
+print "La suma es: $suma"."<br>";
+print "La resta es: $resta"."<br>";
+print "La multiplicación es: $multiplicacion"."<br>";
+print "La división entera es: $division_entera"."<br>";
+print "El resto de la división entera es: $resto"."<br>";
+// Ejercicio 4
+$celsius = 20;
+$fahrenheit = ($celsius * 9/5) + 32;
+print "20°C equivale a $fahrenheit"."<br>";
+// Ejercicio 5
+$base = 18;
+$altura = 12;
+$perimetro_rectan = 2 * ($base + $altura);
+$area_rectan = $base * $altura;
+print "El perímetro del rectángulo es: $perimetro_rectan cm"."<br>";
+print "El área del rectángulo es: $area_rectan cm²"."<br>";
+// Ejercicio 5
+$radio = 30;
+$pi = 3.1416;
+$perimetro_cir = 2 * $pi * $radio;
+$area_cir = pi * $radio ** 2;
+print "El perímetro del círculo es: $perimetro_cir cm"."<br>";
+print "El área del círculo es: $area_cir cm²"."<br>";
+?>
+
